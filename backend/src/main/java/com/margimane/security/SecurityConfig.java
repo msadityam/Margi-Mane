62dcaabc30cae04898d88cb5f1f59fb24436cf24
Aditya <msadityam@gmail.com>
2026-04-05 23:32:01 +0530

    add jar packaging

--- backend/src/main/java/com/margimane/security/SecurityConfig.java
+++ backend/src/main/java/com/margimane/security/SecurityConfig.java
@@ -36,7 +36,8 @@ public class SecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+
+        configuration.setAllowedOriginPatterns(List.of("*")); // 🔥 FIX
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
